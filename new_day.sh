8bfb69e58e2a675f39e75811338dc45b2e5cd46b
Jakob Erzar <jakob.erzar@gmail.com>
2022-12-03 09:14:14 +0100

    Fix new day script

--- new_day.sh
+++ new_day.sh
@@ -15,7 +15,7 @@ touch "$INPUT_DAY_FOLDER/full.txt"
 echo "Input files created"
 
 # Make Kotlin files
-cp "$KOTLIN_FOLDER/Day01.kt" "$KOTLIN_FOLDER/Day$DAY_NAME.py"
+cp "$KOTLIN_FOLDER/Day01.kt" "$KOTLIN_FOLDER/Day$DAY_NAME.kt"
 # cp "$KOTLIN_FOLDER/day-boilerplate.kt" "$KOTLIN_FOLDER/Day$DAY_NAME.py"
 # touch "$KOTLIN_FOLDER/Day$DAY_NAME.kt"
 echo "Kotlin files created"
